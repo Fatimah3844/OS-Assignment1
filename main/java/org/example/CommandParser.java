30c98187bdc0e40f9193c2b2ccff47c1d20eb1c0
AY404 <153795788+AY404@users.noreply.github.com>
2024-10-30 22:46:21 +0300

    Update CommandParser.java
    displayHelp( )  added

--- main/java/org/example/CommandParser.java
+++ main/java/org/example/CommandParser.java
@@ -71,4 +71,21 @@ public class CommandParser {
         }
         scanner.close();
     }
+        private static void displayHelp() {
+        System.out.println("Available Commands:");
+        System.out.println("pwd          : Print current directory");
+        System.out.println("cd <dir>     : Change directory");
+        System.out.println("ls           : List files in the current directory");
+        System.out.println("mkdir <dir>  : Create new directory");
+        System.out.println("rmdir <dir>  : Remove directory");
+        System.out.println("touch <file> : Create empty file");
+        System.out.println("mv <src> <dst>: Move or rename a file");
+        System.out.println("rm <file>    : Remove a file");
+        System.out.println("cat <file>   : Display file content");
+        System.out.println("> <file>     : Redirect output to a file");
+        System.out.println(">> <file>    : Append output to a file");
+        System.out.println("|            : Pipe commands");
+        System.out.println("exit         : Exit the CLI");
+    }
+
 }
